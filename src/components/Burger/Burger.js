f5473fb025380da317f4e91e0b66acab5ceafa53
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-02 14:45:55 +0100

    fixedb propTypes in BurgerIngredient and added burger to App.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import style from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = props => {
     return(
-
+        <div className={style.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="meat"/>
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
     )
 }
 
