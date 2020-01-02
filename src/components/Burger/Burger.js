e9c174aeae5320d0bef7f4ad4e7575a81e8971ad
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-02 15:12:08 +0100

    outputting burger ingr dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,6 +3,11 @@ import style from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = props => {
+    const transformedIngredients = Object.keys(props.ingredients).map(el => {
+        return [...Array(props.ingredients[el])].map((_, i) => {
+            <BurgerIngredient key={el + 1} type={el}/>
+        })
+    })
     return(
         <div className={style.Burger}>
             <BurgerIngredient type="bread-top"/>
