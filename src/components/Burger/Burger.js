5b9fc990c803652de6c5c3c689f7d24048a98008
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-02 15:30:22 +0100

    information when there is no ingredients added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import style from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import { array } from 'prop-types';
 
 const Burger = props => {
-    const transformedIngredients = Object.keys(props.ingredients).map(el => {
+    let transformedIngredients = Object.keys(props.ingredients).map(el => {
         return [...Array(props.ingredients[el])].map((_, i) => {
             return <BurgerIngredient key={el + 1} type={el}/>
         })
-    })
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, [])
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>
+
+    }
+
+    console.log(transformedIngredients)
     return(
         <div className={style.Burger}>
             <BurgerIngredient type="bread-top"/>
