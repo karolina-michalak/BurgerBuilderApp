59d86d0253fc4725e3df297bab126fe563e69a13
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-04 23:07:21 +0100

    mobile backdrop hides when clicking page

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,10 +6,14 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import ReactAux from '../../../hoc/ReactAux'
 
 const SideDrawer = props => {
+    let attachedClasses = [style.SideDrawer, style.Close]
+    if(props.open){
+        attachedClasses = [style.SideDrawer, style.Open]
+    }
     return(
         <ReactAux>
             <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={style.SideDrawer}>
+            <div className={attachedClasses.join(' ')}>
                 <div className={style.Logo}>
                     <Logo/>
                 </div>
