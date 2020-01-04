7f09bd1b373e6459e08e9adf7f8b4cd22264d8d2
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-04 22:13:47 +0100

    filled SideDrawer.js and added styles

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import style from './SideDrawer.module.css'
 
 const SideDrawer = props => {
-    return{
-        
-    }
+    return(
+        <div className={style.SideDrawer}>
+            <Logo/>
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </div>
+    )
 }
 
 export default SideDrawer
