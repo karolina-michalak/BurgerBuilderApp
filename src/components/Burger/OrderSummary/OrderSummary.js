ea18a75246accb29f2a65da14e8e5979e2f3ea4a
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-05 11:59:47 +0100

    fixed order summary class component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,8 +4,8 @@ import Button from '../../../components/UI/Button/Button'
 
 class OrderSummary extends Component{
     render(){
-        const ingredientSummary = Object.keys(props.ingredients).map(el => {
-            return (<li key={el}><span style={{textTransform: 'capitalize'}}>{el}</span>: {props.ingredients[el]}</li>)
+        const ingredientSummary = Object.keys(this.props.ingredients).map(el => {
+            return (<li key={el}><span style={{textTransform: 'capitalize'}}>{el}</span>: {this.props.ingredients[el]}</li>)
         })
         return(
             <ReactAux>
@@ -14,10 +14,10 @@ class OrderSummary extends Component{
                 <ul>
                     {ingredientSummary}
                 </ul>
-                <p><strong>Total price: {props.price.toFixed(2)}</strong></p>
+                <p><strong>Total price: {this.props.price.toFixed(2)}</strong></p>
                 <p>Continue to checkout?</p>
-                <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-                <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
             </ReactAux>
         )
     }
