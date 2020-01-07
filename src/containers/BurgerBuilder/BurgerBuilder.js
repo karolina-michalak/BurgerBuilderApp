29554f99d5889d78569df977bda7a103b5595e68
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-07 22:57:33 +0100

    added random data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
+
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -87,7 +89,21 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinue = () => {
-        alert('You continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Kitty Kat',
+                address: {
+                    street: 'Emerson St',
+                    zipCode: '5454',
+                    country: 'USA'
+                },
+                email: 'example@example.com',
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json')
     }
 
     render(){
