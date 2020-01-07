993c350f4858f29b4a7d1c5c47abacc874e9acfc
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-07 23:01:47 +0100

    sending order data to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -103,7 +103,9 @@ class BurgerBuilder extends Component{
             },
             deliveryMethod: 'fastest'
         }
-        axios.post('/orders.json')
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
     }
 
     render(){
