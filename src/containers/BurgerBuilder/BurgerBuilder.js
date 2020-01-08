0718b8e63928c5d6753ede457fecedd2768515e1
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-08 22:59:31 +0100

    fixed error

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -108,7 +108,7 @@ class BurgerBuilder extends Component{
             },
             deliveryMethod: 'fastest'
         }
-        axios.post('/orders', order)
+        axios.post('/orders.json', order)
             .then(response => {
                 this.setState({
                     loading: false,
