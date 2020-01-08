eff4d617b19372d3f175d4d9188651f762505c1a
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-08 22:58:19 +0100

    handling errors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,11 +4,35 @@ import ReactAux from '../ReactAux'
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component{
+        state = {
+            error: null
+        }
+        componentDidMount(){
+            axios.interceptors.request.use(req => {
+                this.setState({
+                    error: null
+                })
+                return req
+            })
+            axios.interceptors.response.use(res => res, error => {
+                this.setState({
+                    error: error
+                })
+            })
+        }
+
+        errorHandler = () => {
+            this.setState({
+                error: null
+            })
+        }
         render(){
             return(
             <ReactAux>
-                <Modal show>
-                    Sth didn't work!
+                <Modal 
+                    show={this.state.error}
+                    clicked={this.errorHandler}>
+                    {this.state.error ? this.state.error.message : null}
                 </Modal>
                 <WrappedComponent {...this.props}/>
             </ReactAux>
