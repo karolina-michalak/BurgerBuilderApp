c0efdd824b9fd27f8d27efc66edb4d4392a45acb
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-08 22:45:44 +0100

    changed withErrorHandler to class component

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import ReactAux from '../ReactAux'
 
-const withErrorHandler = WrappedComponent => {
-    return (props) => {
-        return(
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component{
+        render(){
+            return(
             <ReactAux>
                 <Modal show>
                     Sth didn't work!
                 </Modal>
-                <WrappedComponent {...props}/>
+                <WrappedComponent {...this.props}/>
             </ReactAux>
-        )
+            )
+        }
     }
 }
-
 export default withErrorHandler
