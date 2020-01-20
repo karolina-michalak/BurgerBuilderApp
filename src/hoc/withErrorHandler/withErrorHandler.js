5eb9778a4c65ea83276fd1e1dded55bd2131d2b0
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-20 23:33:50 +0100

    removing intreceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,13 +8,13 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
         componentWillMount(){
-            axios.interceptors.request.use(req => {
+            this.requestIntreceptor = axios.interceptors.request.use(req => {
                 this.setState({
                     error: null
                 })
                 return req
             })
-            axios.interceptors.response.use(res => res, error => {
+            this.resIntreceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({
                     error: error
                 })
@@ -22,7 +22,9 @@ const withErrorHandler = (WrappedComponent, axios) => {
         }
 
         componentWillUnmount(){
-            
+            axios.interceptors.request.eject(this.requestIntreceptor)
+            axios.interceptors.response.eject(this.resIntreceptor)
+
         }
 
         errorHandler = () => {
